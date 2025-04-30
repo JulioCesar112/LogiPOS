dbf1642196e41724589a9c1308b74fedb029c0ec
Julio Cesar <billyjoplin112@gmail.com>
2025-04-30 11:14:33 -0600

    Agrega soporte para conexión remota y local a la base de datos

--- src/app.js
+++ src/app.js
@@ -6,55 +6,55 @@
   - db: database connection
 */
 
-const express = require("express")
-const cors = require("cors")
-const config = require("./config/env")
-const db = require("./config/database")
+const express = require("express");
+const cors = require("cors");
+const config = require("./config/env");
+const db = require("./config/database");
 
 /*
   Router imports:
   - userRouter: routes for user management
   - authRouter: routes for user authentication
 */
-const userRouter = require("./routes/userRouter")
-const authRouter = require("./routes/authRouter")
-const categoryRouter = require("./routes/categoryRoute")
-const productRouter = require("./routes/productRouter")
-const saleRouter = require("./routes/saleRouter")
-const initModels = require("./models/initModels")
+const userRouter = require("./routes/userRouter");
+const authRouter = require("./routes/authRouter");
+const categoryRouter = require("./routes/categoryRoute");
+const productRouter = require("./routes/productRouter");
+const saleRouter = require("./routes/saleRouter");
+const initModels = require("./models/initModels");
 
-const app = express()
-const apiRouter = express.Router()
+const app = express();
+const apiRouter = express.Router();
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // Rutes
-apiRouter.use("/auth",authRouter)
-apiRouter.use("/categories", categoryRouter)
-apiRouter.use("/users", userRouter)
-apiRouter.use("/products",productRouter),
-apiRouter.use("/sales", saleRouter)
+apiRouter.use("/auth", authRouter);
+apiRouter.use("/categories", categoryRouter);
+apiRouter.use("/users", userRouter);
+apiRouter.use("/products", productRouter), apiRouter.use("/sales", saleRouter);
 
-app.use("/api/v1",apiRouter)
-
+app.use("/api/v1", apiRouter);
 
 // Connection to the database
 const initDatabase = async () => {
   try {
     await db.authenticate();
-    console.log('Connection to the database has been established successfully.');
-    await db.sync({ alter: false });
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+    await initModels();
+    await db.sync({ alter: true });
     console.log("DB Synced");
-    await initModels()
   } catch (err) {
-    console.error('Unable to connect to the database:', err);
+    console.error("Unable to connect to the database:", err);
   }
 };
 
-initDatabase()
+initDatabase();
 //app listen
 app.listen(config.port, () => {
-  console.log(`Server Started At Port: ${config.port}`)
-})
+  console.log(`Server Started At Port: ${config.port}`);
+});
--- src/config/env.js
+++ src/config/env.js
@@ -1,18 +1,17 @@
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 const config = {
-  port:process.env.PORT || 9000,
+  port: process.env.PORT || 9000,
   nodeEnv: process.env.NODE_ENV || "development",
+  db_url: process.env.DATABASE_URL,
   db: {
     host: process.env.DB_HOST || "localhost",
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASS || "root",
-    dbName: process.env.DB_NAME 
-
+    dbName: process.env.DB_NAME,
   },
-  jwtsecret: process.env.JWT_SECRET
-}
+  jwtsecret: process.env.JWT_SECRET,
+};
 
-
-module.exports = config
+module.exports = config;
